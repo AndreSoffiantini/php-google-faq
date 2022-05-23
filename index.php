6dd7a96447df31642c3f7d529a08718352043f91
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-05-23 16:21:11 +0200

    Add navbar

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
+    $nav_items = [
+        "Introduzione",
+        "Norme sulla privacy",
+        "Termini di servizio",
+        "Tecnologie",
+        "Domande frequenti"
+    ];
+
     $faqs = [
 
         [
@@ -62,12 +70,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Google faq</title>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
-    <link rel='stylesheet' href='./assets/css/style.css'>
+    <link rel='stylesheet' href='./assets/scss/style.scss'>
 </head>
 <body>
     
-    <header id="site_header">
-        <img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png" alt="">
+    <header class="px-3 pt-2 border-bottom border-secondary" id="site_header">
+        
+        <div class="logo_title d-flex align-items-center mb-3">
+            <img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png" alt="">
+            <h3 class="mx-3">Privacy & Termini</h3>
+        </div>
+
+        <nav>
+            <ul class="list-group list-group-horizontal">
+                <?php foreach($nav_items as $nav_item) : ?>
+                    <li class="list-group-item border-white ps-0"> <a href="" class="text-decoration-none text-black"> <?= $nav_item ?> </a> </li>
+                <?php endforeach ?> 
+            </ul>
+        </nav>
+        
     </header>
     <!-- /#site_header -->
 
